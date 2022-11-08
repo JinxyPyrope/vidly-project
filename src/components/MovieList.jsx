3c61aeb0589ed412d9beab026b397164e0144d31
Nhoj <nhojplummer@yahoo.com>
2022-11-07 22:04:35 -0500

    Up to displaying pages

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from "react"
 import { getMovies } from "../services/fakeMovieService"
 import Like from "./common/like"
+import Pagination from "./common/pagination"
 class Movies extends Component {
   state = {
-    movies: getMovies()
+    movies: getMovies(),
+    pageSize: 4
   }
 
   handleDelete = movie => {
@@ -17,6 +19,10 @@ class Movies extends Component {
     movies[index] = { ...movies[index] }
     movies[index].liked = !movies[index].liked
     this.setState({ movies })
+  }
+
+  handlePageChange = page => {
+    console.log(page)
   }
 
   render() {
@@ -55,6 +61,7 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+        <Pagination itemsCount={count} pageSize={10} onPageChange={this.handlePageChange} />
       </React.Fragment>
     )
   }
