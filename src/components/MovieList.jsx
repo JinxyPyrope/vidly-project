56b13a1bba617fa10e0c9d8dca03d10b376dff53
Nhoj <nhojplummer@yahoo.com>
2022-11-07 20:47:53 -0500

    Adding more to the coding section

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import { getMovies } from "../services/fakeMovieService"
-
+import Like from "./common/like"
 class Movies extends Component {
   state = {
     movies: getMovies()
@@ -9,6 +9,14 @@ class Movies extends Component {
   handleDelete = movie => {
     const movies = this.state.movies.filter(m => m._id !== movie._id)
     this.setState({ movies: movies })
+  }
+
+  handleLike = movie => {
+    const movies = [...this.state.movies]
+    const index = movies.indexOf(movie)
+    movies[index] = { ...movies[index] }
+    movies[index].liked = !movies[index].liked
+    this.setState({ movies })
   }
 
   render() {
@@ -35,6 +43,9 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like liked={movie.liked} onClick={() => this.handleLike(movie)} />
+                </td>
                 <td>
                   <button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">
                     Delete
